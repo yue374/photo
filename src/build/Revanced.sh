d6d34c5bdc261ff5f960042b9cf0ff2bab6e0f46
yue374 <139092243+yue374@users.noreply.github.com>
2025-05-25 21:38:35 +0700

    Update Revanced.sh

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -48,6 +48,7 @@ revanced_dl(){
 	# Patch Google photos:
 	# Arm64-v8a
 	get_patches_key "gg-photos"
+ 	version="6.94.0.662644291"
 	get_apk "com.google.android.apps.photos" "gg-photos-arm64-v8a" "photos" "google-inc/photos/google-photos" "arm64-v8a" "nodpi"
 	patch "gg-photos-arm64-v8a" "revanced"
 	# Armeabi-v7a
